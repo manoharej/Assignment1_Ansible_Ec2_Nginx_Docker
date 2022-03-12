0f9c61ba3542b45e269988eb6a58cbf74d38cb26
manoharej <76063339+manoharej@users.noreply.github.com>
2022-03-12 14:38:43 +0530

    Delete user_data.sh

--- user_data.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-sudo yum install git -y
-sudo yum install docker -y
-sudo amazon-linux-extras install nginx1 -y
-sudo service nginx start
-sudo service docker start
-sudo git clone https://github.com/typicode/json-server.git
-sudo docker build -t testimg -f /json-server/Dockerfile
-sudo docker -d --name testnodeapp -p 49160:8080 testimg
